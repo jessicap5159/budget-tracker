9cb7b1aee4cb239de3aa9f806690483ad5a6d4f6
jessicap5159 <jessicaj5159@gmail.com>
2021-06-16 18:43:06 -0700

    added web manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,4 @@
+const { get } = require("mongoose");
 // variable to hold db connection
 let db;
 // establish connection to IndexedDB database called "budget" and set to version 1
@@ -27,4 +28,16 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
+}
+
+// This function will be executed if we attempt to submit new data
+function saveRecord(record) {
+    // open new transaction with database with read and write permissions
+    const transaction = db.transaction(['new_data'], 'readwrite');
+
+    // access the object store for `new_data`
+    const budgetObjectStore = transaction.objectStore('new_data');
+
+    // add record to store with add method
+    budgetObjectStore.add(record);
 }
